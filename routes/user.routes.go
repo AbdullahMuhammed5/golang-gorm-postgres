75e7cc46679147919b3aa5cd03596936f21899d0
abdullahmuhammed5 <abdo56668@gmail.com>
2023-03-08 19:41:32 +0200

    add JWT Authentication

---
+++ routes/user.routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/abdullahmuhammed5/golang-gorm-postgres/controllers"
+	"github.com/abdullahmuhammed5/golang-gorm-postgres/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type UserRouteController struct {
+	userController controllers.UserController
+}
+
+func NewRouteUserController(userController controllers.UserController) UserRouteController {
+	return UserRouteController{userController}
+}
+
+func (uc *UserRouteController) UserRoute(rg *gin.RouterGroup) {
+	router := rg.Group("users")
+	router.GET("/me", middleware.DeserializeUser(), uc.userController.GetMe)
+}
